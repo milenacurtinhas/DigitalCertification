2de030cecad796ff7161a75310134ca71e6bef61
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-11-21 11:25:31 -0300

    Adição de omentários

--- tarefa1/files_manager.py
+++ tarefa1/files_manager.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 from cryptography.hazmat.primitives import serialization
 
+
 def create_folder(folder_name):
+    """
+    Função que cria uma subpasta dentro de um diretório pré-definido ('tarefa1/ca_storage').
+    :param folder_name: nome da pasta a ser criada
+    :return: retorna o caminho da nova pasta
+    """
     folder_path = Path(f'tarefa1/ca_storage/{folder_name}')
 
     try:
@@ -14,6 +20,14 @@ def create_folder(folder_name):
     return folder_path
 
 def save_pem(file, folder_path, file_name, extension):
+    """
+    Função que salva em disco um objeto (Certificado ou CSR) codificado em PEM.
+    A extensão é recebida por parâmetro (ex: 'csr', 'crt', 'pem').
+    :param file: arquivoa ser codificado
+    :param folder_path: caminho da pasta onde a chave será salva
+    :param file_name: nome do arquivo a ser salvo
+    :param extension: extensão do arquivo a ser salvo 
+    """
     file_path = folder_path/f'{file_name}.{extension}'
     try:
         with open(file_path, 'wb') as f:
@@ -22,6 +36,14 @@ def save_pem(file, folder_path, file_name, extension):
         print(f"An error occurred while saving {file_path}: {e}")
 
 def save_key(key, folder_path, file_name):
+    """
+   Função que salva em disco a chave privada codificada em PEM, 
+    usando o formato tradicional do OpenSSL e sem criptografia.
+    A extensão é fixa ('key').
+    :param key: chave privada a ser salva
+    :param folder_path: caminho da pasta onde a chave será salva
+    :param file_name: nome do arquivo (sem extensão)
+    """
     file_path = folder_path/f'{file_name}.key'
     try:
         with open(file_path, 'wb') as f:
